2d9963cdf99ca408b66f5d181abd717d8a7064fb
NakagawaYuuto <github.nakagawa@gmail.com>
2022-11-06 22:38:49 +0900

    タスク管理アプリケーション　模写

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -14,12 +13,12 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Controller
-public class HomeRstController {
+public class HomeRestController {
 
     record TaskItem(String id, String task, String deadline, boolean done) {}
     private List<TaskItem> taskItems = new ArrayList<>();
 
-    @RequestMapping(value="/restthehello")
+    @RequestMapping(value="/resthello")
     String hello() {
         return """
                 Hello.
@@ -28,7 +27,7 @@ public class HomeRstController {
                 """.formatted(LocalDateTime.now());
     }
 
-//    @GetMapping("/restadd") //@RequestMapping(value="/restadd", method=RequestMethod.GET)
+//    @GetMapping("/restadd") //　＝　@RequestMapping(value="/restadd", method=RequestMethod.GET)　というように記述したと同じ
 //    String addItem(@RequestParam("task") String task,
 //                   @RequestParam("deadline") String deadline) {
 //        String id = UUID.randomUUID().toString().substring(0, 8);
@@ -44,21 +43,5 @@ public class HomeRstController {
                 .map(TaskItem::toString)
                 .collect(Collectors.joining(", "));
         return result;
-    }
-
-    @GetMapping(value = "/list")
-    String listItems(Model model) {
-        model.addAttribute("tasklist", taskItems);
-        return "home";
-    }
-
-    @GetMapping("/add")
-    String addItem(@RequestParam("task") String task,
-                   @RequestParam("deadline") String deadline) {
-        String id = UUID.randomUUID().toString().substring(0, 8);
-        TaskItem item = new TaskItem(id, task, deadline, false);
-        taskItems.add(item);
-
-        return "redirect:/list";
     }
 }
